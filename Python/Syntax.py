4c7dc0fc41aa478d7225afca5078122be6f0f0d0
Mohamed Ibrahim <mohamedgazia2003@gmail.com>
2025-10-03 18:25:17 +0300

    Added Syntax.py
    Added Comments.py

---
+++ Python/Syntax.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------------
+# ------------------- Syntax -------------------
+# -------------------------------------------------------------------------------
+# Python syntax depend on Indentation.
+# Indentation: is the number of spaces used at the beginning of a line to define code 
+# blocks, such as the content of conditional statements, loops, functions, and classes.
+# -------------------------------------------------------------------------------
+
+# Example
+if True:
+    print("Hello.")
+    print("I am in if statement.")
+print("I am not in if statement.")
+
+# Example 2 (This gives error)
+# if True:
+#     print("Hello.")
+#      print("I am in if statement.")
