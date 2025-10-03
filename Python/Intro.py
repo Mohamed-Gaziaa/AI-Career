1bff24b6d241bd29a5da54b4d16d5ae942c3516e
Mohamed Ibrahim <mohamedibrahimgaziaa1503@gmail.com>
2025-10-03 17:35:48 +0300

    Add Intro.py and First_app.py files.

---
+++ Python/Intro.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------
+# -------- What is python? --------
+# ----------------------------------------------------------------------
+# 1) Programming language that can do anything. 
+# 2) Free and open source.
+# 3) Interpreted (Executed line by line).
+# 4) Interactive (The output appears directly in the terminal).
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# -------- Why python? --------
+# ----------------------------------------------------------------------
+# 1) Easy to install.
+# 2) Clean and easy to learn.
+# 3) Error handling.
+# 4) Debugging is easy.
+# 5) Cross platform (Deal with any operating system like Windows, Mac, etc..).
+# 6) OOP (Object Orianted Programming).
+# 7) Integrated (Enter with any other programming language).
+# 8) Support module and packages.
+# 9) Memory management.
+# 10) Multi purpose.
+# 11) Growing fast.
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# -------- What is python used for? --------
+# ----------------------------------------------------------------------
+# 1) Web development (Django, Flask).
+# 2) Games (Pygame).
+# 3) Desktop apps (PyGUI, Tkinter).
+# 4) Hacking.
+# 5) Machine learning and Data science.
+# 6) AI and Robots.
+# 7) Automation.
+# 8) Web scrapping.
+# 9) Android games.
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# -------- What we need to learn python? --------
+# ----------------------------------------------------------------------
+# 1) Text editor (ex: VS Code) or IDE (Integrated Development Environment) (ex: PyCharm). 
+# 2) Download python.
+# 3) Add python extension.
+# ----------------------------------------------------------------------
