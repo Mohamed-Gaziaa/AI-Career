94d8df87469d22332b857fea9e27f93baf3bd5bf
Mohamed Ibrahim <mohamedgazia2003@gmail.com>
2025-10-07 19:32:23 +0300

    Added variables.py file and modify README.md

---
+++ Python/Variables.py
@@ -0,0 +1,48 @@
+# -------------------------------------------------------------------------------
+# ------------------- Variables -------------------
+# -------------------------------------------------------------------------------
+# Syntax => [Variable Name] [Assignment Operator] [Value]
+# -------------------------------------------------------------------------------
+# Name convention and rules
+# [1] Can contain letters, digits, underscore(_).
+# [2] Cannot contain special characters(!, @, #, $, %, ^, &, *, (, ), -).
+# [3] Cannot start with digit or special characters. 
+# [4] Case sensitive (Name not equal name).
+# [5] Should be descriptive.
+# -------------------------------------------------------------------------------
+
+name = "Mohamed Ibrahim"
+print(name)
+print("-------------------------------------------------------------------------------")
+
+# -------------------------------------------------------------------------------
+# Variable naming styles (naming cases)
+# [1] Snake case (Most common using in python).
+# [2] Camel case.
+# [3] Pascal case.
+# [4] Upper case.
+# -------------------------------------------------------------------------------
+
+# Snake case.
+user_name = "Mohamed"
+total_price = 250
+
+# Camel case.
+userName = "Mohamed"
+totalPrice = 250
+
+# Pascal case.
+UserName = "Mohamed"
+TotalPrice = 250
+
+# Upper case.
+USERNAME = "Mohamed"
+TOTALPRICE = 250
+
+# help("keywords") => The reserved words in python 
+
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
--- README.md
+++ README.md
@@ -3,3 +3,12 @@ This is my AI journey.
 
 1) I added the Git&Github course.
 2) I started the Python course.
+
+i will add it later
+--------------------------------------------------------
+Source Code : Original Code You Write it in Computer
+Translation : Converting Source Code Into Machine Language
+Compilation : Translate Code Before Run Time
+Run-Time : Period App Take To Executing Commands
+Interpreted : Code Translated On The Fly During Execution
+--------------------------------------------------------
