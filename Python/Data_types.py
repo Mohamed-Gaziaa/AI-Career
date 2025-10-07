3fd69187640ef2c625e276333846082cbc2816fe
Mohamed Ibrahim <mohamedgazia2003@gmail.com>
2025-10-07 18:57:06 +0300

    Added the Data_types.py file

---
+++ Python/Data_types.py
@@ -0,0 +1,32 @@
+# -------------------------------------------------------------------------------
+# ------------------- Data types -------------------
+# -------------------------------------------------------------------------------
+# type() => Function used to detect the object data type.
+# All data in python is object.
+# -------------------------------------------------------------------------------
+
+# int => Integer (All the numbers from '- infinity' to '+ infinity')
+print(type(10))
+print(type(100))
+print(type(-50))
+
+# float => Floating Point Number (All the decimals from '- infinity' to '+ infinity')
+print(type(100.9))
+print(type(1.950950))
+print(type(-100.9595))
+
+# str => String (Any sequence of characters) (Any thing print between '' or "")
+print(type("Hello Python"))
+
+# list => List "[]"
+print(type([1, 2, 3, 4, 5]))
+
+# tuple => Tuple "()"
+print(type((1, 2, 3, 4, 5)))
+
+# dict => Dictionary "{}"
+print(type({"One": 1, "Two": 2, "Three": 3}))
+
+# bool => Boolean (True or False)
+print(type(2 == 2))
+print(type(2 == 3))
