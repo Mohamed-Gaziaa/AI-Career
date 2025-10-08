0ef6e8bf474940ba36b06e7aa151e39688613e51
Mohamed Ibrahim <mohamedgazia2003@gmail.com>
2025-10-08 18:17:23 +0300

    Added Concatenation.py file

---
+++ Python/Concatenation.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------------
+# ------------------------- Concatenation -------------------------
+# -------------------------------------------------------------------------------
+# - Concatenation => Add two strings together to make a larger one.
+# - We can't concatenate string to anyother datatype, we just can concatenate 
+# string to string. (String + String)
+# -------------------------------------------------------------------------------
+
+first_name = "Mohamed"
+second_name = "Ibrahim"
+
+print(first_name + second_name)
+print(first_name +" "+ second_name)
+
+full_name = first_name +" "+ second_name
+
+print(full_name)
+
+# print("Mohamed" + 22)  # Error
