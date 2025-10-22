0c27cf6146be95be63797e4b0f26785130558e9c
Mohamed Ibrahim <mohamedgazia2003@gmail.com>
2025-10-22 16:46:08 +0300

    Add Assignment_operators.py and Comparison_operators.py

---
+++ Python/Comparison_operators.py
@@ -0,0 +1,54 @@
+# -------------------------------------------------------------------------------
+# -------------------------  Comparison operators -------------------------
+# -------------------------------------------------------------------------------
+# [ == ] Equal
+# [ != ] Not equal
+# [ > ] Greater than
+# [ < ] Less than
+# [ >= ] Greater than or equal
+# [ <= ] Less than or equal
+# -------------------------------------------------------------------------------
+
+# Equal and not equal
+
+print(100 == 100)
+print(100 == 200)
+print(100 == 100.00)
+
+print("#" * 50)
+
+print(100 != 100)
+print(100 != 200)
+print(100 != 100.00)
+
+print("#" * 50)
+
+# Greater than and less than
+
+print(100 > 100)
+print(100 > 200)
+print(100 > 100.00)
+print(100 > 40)
+
+print("#" * 50)
+
+print(100 < 100)
+print(100 < 200)
+print(100 < 100.00)
+print(100 < 40)
+
+print("#" * 50)
+
+# Greater than or equal and less than or equal
+
+print(100 >= 100)
+print(100 >= 200)
+print(100 >= 100.00)
+print(100 >= 40)
+
+print("#" * 50)
+
+print(100 <= 100)
+print(100 <= 200)
+print(100 <= 100.00)
+print(100 <= 40)
